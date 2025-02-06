6eae46a2b1341bc7addab2ae900471c6ed5b8e1f
dilyabareeva <dilyabareeva@gmail.com>
2025-02-06 22:54:44 +0100

    find best alpha for broccoli bird

--- slurm/start_batch_slurm.sh
+++ slurm/start_batch_slurm.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 #apptainer build --fakeroot --force ./slingshot_pre_build.sif ./grad-slingshot/slurm/base_build.def
-#apptainer build --fakeroot --force ./slingshot.sif ./grad-slingshot/slurm/batch_slurm.def
-for batch_size in 32; do
-  for noise_batch_size in 32; do
-    sbatch ./grad-slingshot/slurm/batch_slurm.sbatch "${batch_size}" "${noise_batch_size}"
-  done
+apptainer build --fakeroot --force ./slingshot.sif ./grad-slingshot/slurm/batch_slurm.def
+for alpha in 0.0 1e-5 5e-5 1e-4 5e-4 1e-3 5e-3 1e-2 5e-2 1e-1 0.5 0.9; do
+    sbatch ./grad-slingshot/slurm/batch_slurm.sbatch "${alpha}"
 done
 
 #1539655
