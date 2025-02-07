3945fefe64d1543703170887efdd0acba104db4d
dilyabareeva <dilyabareeva@gmail.com>
2025-02-07 11:02:37 +0100

    experiment config reading

--- experiments/batch_man_visualization.py
+++ experiments/batch_man_visualization.py
@@ -2,13 +2,20 @@ from hydra import initialize, compose
 
 from experiments.visualize_manipulation import viz_manipulation
 
+"""
+param_grid = {
+    "alpha": [0.0, 1e-5, 5e-5, 1e-4, 5e-4, 1e-3, 5e-3, 1e-2, 5e-2, 1e-1, 0.5,
+              0.9],
+    "img_str": ["inet_val_ILSVRC2012_val_00023907_max_act", "tractor"],
+}
+"""
 
-def batch_man_viz(param_grid):
+def batch_man_viz(param_grid, cfg_path="../config", cfg_name="config"):
     for param in param_grid:
         for param_value in param_grid[param]:
-            with initialize(version_base=None, config_path="../config"):
+            with initialize(version_base=None, config_path=cfg_path):
                 cfg = compose(
-                    config_name="config",
+                    config_name=cfg_name,
                     overrides=[
                         f"{param}={param_value}",
                         # f"data={data}",
@@ -22,7 +29,10 @@ def batch_man_viz(param_grid):
             viz_manipulation(cfg)
 
 if __name__ == "__main__":
+    cfg_name = "./configs"
+    cfg_path = "config_broccoli_bird_tractor"
     param_grid = {
         "alpha": [0.0, 1e-5, 5e-5, 1e-4, 5e-4, 1e-3, 5e-3, 1e-2, 5e-2, 1e-1, 0.5, 0.9],
+        "img_str": ["inet_val_ILSVRC2012_val_00023907_max_act", "tractor"],
     }
     batch_man_viz(param_grid)
