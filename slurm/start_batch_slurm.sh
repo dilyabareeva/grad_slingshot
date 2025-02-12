bdb63080b593498edc0d30979df56995b3ce09ba
dilyabareeva <dilyabareeva@gmail.com>
2025-02-12 21:26:09 +0100

    many img experiment add ACTUAL train and test images

--- slurm/start_batch_slurm.sh
+++ slurm/start_batch_slurm.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 #apptainer build --fakeroot --force ./slingshot_pre_build.sif ./grad-slingshot/slurm/base_build.def
 apptainer build --fakeroot --force ./slingshot.sif ./grad-slingshot/slurm/batch_slurm.def
-for kernel_size in 224; do
-  for inplanes in 3 12; do
-    sbatch ./grad-slingshot/slurm/batch_slurm.sbatch "${kernel_size}" "${inplanes}"
+for relu in "True" "False"; do
+  for target_img_path in './assets/image_dep/train_example_0.png' \
+                        './assets/image_dep/train_example_1.png' \
+                        './assets/image_dep/train_example_2.png' \
+                        './assets/image_dep/test_example_0.png' \
+                        './assets/image_dep/test_example_1.png' \
+                        './assets/image_dep/test_example_2.png'; do
+    sbatch ./grad-slingshot/slurm/batch_slurm.sbatch "${relu}" "${target_img_path}"
   done
 done
 
