02f50c19cfcfe1984f50db309b1e74e434be9296
dilyabareeva <dilyabareeva@gmail.com>
2025-02-13 21:11:49 +0100

    big kernel experiment re-run

--- slurm/start_batch_slurm.sh
+++ slurm/start_batch_slurm.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 #apptainer build --fakeroot --force ./slingshot_pre_build.sif ./grad-slingshot/slurm/base_build.def
 apptainer build --fakeroot --force ./slingshot.sif ./grad-slingshot/slurm/batch_slurm.def
-for relu in "True" "False"; do
-  for target_img_path in './assets/image_dep/train_example_0.png' \
-                        './assets/image_dep/train_example_1.png' \
-                        './assets/image_dep/train_example_2.png' \
-                        './assets/image_dep/test_example_0.png' \
-                        './assets/image_dep/test_example_1.png' \
-                        './assets/image_dep/test_example_2.png'; do
-    sbatch ./grad-slingshot/slurm/batch_slurm.sbatch "${relu}" "${target_img_path}"
+for kernel_size in 224; do
+  for inplanes in 3 12; do
+    sbatch ./grad-slingshot/slurm/batch_slurm.sbatch "${kernel_size}" "${inplanes}"
   done
 done
 
