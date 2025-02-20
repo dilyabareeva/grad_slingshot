1436dac01ab48099ce4906ff4d63528cafde8e41
dilyabareeva <dilyabareeva@gmail.com>
2025-02-20 16:32:42 +0100

    add dalmatian slurm

--- slurm/experiment_catfish.sh
+++ slurm/experiment_catfish.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#apptainer build --fakeroot --force ./slingshot_pre_build.sif ./grad-slingshot/slurm/base_build.def
-#apptainer build --fakeroot --force ./slingshot.sif ./grad-slingshot/slurm/batch_slurm.def
 for key in A B C D; do
   for width in "8" "16" "32" "64"; do
     sbatch ./grad-slingshot/slurm/catfish.sbatch "${key}" "${width}"
