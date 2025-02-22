959cc1109692c52a80df8a478e53510d077e4cb7
dilyabareeva <dilyabareeva@gmail.com>
2025-02-22 07:41:52 +0100

    comparison evaluation fixes

--- experiments/prox_pulse_ce.sh
+++ experiments/prox_pulse_ce.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-for alpha in 0.999; do
-  for ce in "True" "False"; do
-      python main.py --config-name=config_rs50_dalmatian_tunnel alpha=${alpha} +prox_pulse=True +prox_pulse_ce=${ce} epochs=5 img_str=dalmatian_prox_pulse_ce
-  done
+for alpha in 1e-3 1e-2 0.05 0.1 0.2 0.5 0.8 0.9 0.95 0.99 0.999; do
+    python main.py --config-name=config_rs50_dalmatian_tunnel alpha=${alpha} +prox_pulse=True +prox_pulse_ce="True" epochs=5 img_str=dalmatian_prox_pulse
 done
-
-
-#1539655
