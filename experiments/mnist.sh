67f654f0226963bf2fa0af0e860dce386603177c
dilyabareeva <dilyabareeva@gmail.com>
2025-02-22 10:01:26 +0100

    add mnist experiments

--- experiments/mnist.sh
+++ experiments/mnist.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-for alpha in 1e-3 1e-2 0.05 0.1 0.2 0.5 0.8 0.9 0.95 0.99; do
-   for replace_relu in "True" "False"; do
-      python main.py --config-name=config_mnist alpha=${alpha} replace_relu=${replace_relu}
-   done
+for gamma in 1.0; do
+  for alpha in 1e-3 1e-2 0.05 0.1 0.2 0.5 0.8 0.9 0.95 0.99; do
+     for replace_relu in "True" "False"; do
+        python main.py --config-name=config_mnist alpha=${alpha} replace_relu=${replace_relu} gamma=${gamma} 2>&1 | tee mnist_${alpha}_${replace_relu}_${gamma}.log
+     done
+  done
 done
