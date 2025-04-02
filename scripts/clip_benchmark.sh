1d00373589e573a875f55e776939e26162367f85
dilyabareeva <dilyabareeva@gmail.com>
2025-04-02 20:39:34 +0200

    🐧figure 1 clip benchmarks

--- scripts/clip_benchmark.sh
+++ scripts/clip_benchmark.sh
@@ -1,36 +1,39 @@
 # Zero-Shot Image Classification: ImageNet
-python3 clip_benchmark/cli.py eval  --model_type "clip_manipulated"  --model "ViT-L/14"  --task "zeroshot_classification"   --dataset "imagenet1k"   --dataset_root "/data1/datapool/"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_{dataset}_{pretrained}_{model_type}_{language}_{task}.json'
+##python3 clip_benchmark/cli.py eval  --model_type "clip_manipulated"  --model "ViT-L/14"  --task "zeroshot_classification"   --dataset "imagenet1k"   --dataset_root "/data1/datapool/"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_{dataset}_{pretrained}_{model_type}_{language}_{task}.json' --device "cuda:0"
 
-#python3 clip_benchmark/cli.py eval  --model_type "clip"  --model "ViT-L/14"  --task "zeroshot_classification"   --dataset "imagenet1k"   --dataset_root "/data1/datapool/"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_{dataset}_{pretrained}_{model_type}_{language}_{task}.json'
+##python3 clip_benchmark/cli.py eval  --model_type "clip"  --model "ViT-L/14"  --task "zeroshot_classification"   --dataset "imagenet1k"   --dataset_root "/data1/datapool/"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_{dataset}_{pretrained}_{model_type}_{language}_{task}.json' --device "cuda:0"
 
-# Zero-Shot Image Classification: Fine-Grained Task (cars classification)
-python3 clip_benchmark/cli.py eval  --model_type "clip"  --model "ViT-L/14"  --dataset=cars --task=zeroshot_classification --dataset_root "/data1/datapool/"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_{dataset}_{pretrained}_{model_type}_{language}_{task}.json'
+# Zero-Shot Image Classification: Eurosat
+python3 clip_benchmark/cli.py eval  --model_type "clip_manipulated"  --model "ViT-L/14"  --task "zeroshot_classification"   --dataset "vtab/eurosat"   --dataset_root "/data1/datapool/"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_{dataset}_{pretrained}_{model_type}_{language}_{task}.json' --device "cuda:0"
 
-python3 clip_benchmark/cli.py eval  --model_type "clip_manipulated"  --model "ViT-L/14"  --dataset=cars --task=zeroshot_classification --dataset_root "/data1/datapool/"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_{dataset}_{pretrained}_{model_type}_{language}_{task}.json'
+python3 clip_benchmark/cli.py eval  --model_type "clip"  --model "ViT-L/14"  --task "zeroshot_classification"   --dataset "vtab/eurosat"   --dataset_root "/data1/datapool/"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_{dataset}_{pretrained}_{model_type}_{language}_{task}.json' --device "cuda:0"
+
+# Zero-Shot Image Classification: STL-10
+python3 clip_benchmark/cli.py eval  --model_type "clip_manipulated"  --model "ViT-L/14"  --task "zeroshot_classification"   --dataset "stl10"   --dataset_root "/data1/datapool/"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_{dataset}_{pretrained}_{model_type}_{language}_{task}.json' --device "cuda:0"
+
+python3 clip_benchmark/cli.py eval  --model_type "clip"  --model "ViT-L/14"  --task "zeroshot_classification"   --dataset "stl10"   --dataset_root "/data1/datapool/"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_{dataset}_{pretrained}_{model_type}_{language}_{task}.json' --device "cuda:0"
+
+# Zero-Shot Image Classification: Fine-Grained Task (pets classification)
+##python3 clip_benchmark/cli.py eval  --model_type "clip"  --model "ViT-L/14"  --dataset=vtab/pets --task=zeroshot_classification --dataset_root "/data1/datapool/"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_{dataset}_{pretrained}_{model_type}_{language}_{task}.json' --device "cuda:0"
+
+##python3 clip_benchmark/cli.py eval  --model_type "clip_manipulated"  --model "ViT-L/14"  --dataset=vtab/pets --task=zeroshot_classification --dataset_root "/data1/datapool/"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_{dataset}_{pretrained}_{model_type}_{language}_{task}.json' --device "cuda:0"
 
 # Zero-Shot Image Classification: More Abstract Task, Describable Textures Dataset (DTD)
-#python3 clip_benchmark/cli.py eval  --model_type "clip"  --model "ViT-L/14"  --dataset=vtab/dtd --task=zeroshot_classification --dataset_root "/data1/datapool/"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_{dataset}_{pretrained}_{model_type}_{language}_{task}.json'
+##python3 clip_benchmark/cli.py eval  --model_type "clip"  --model "ViT-L/14"  --dataset=vtab/dtd --task=zeroshot_classification --dataset_root "/data1/datapool/"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_{dataset}_{pretrained}_{model_type}_{language}_{task}.json' --device "cuda:0"
 
-python3 clip_benchmark/cli.py eval  --model_type "clip_manipulated"  --model "ViT-L/14"  --dataset=vtab/dtd --task=zeroshot_classification --dataset_root "/data1/datapool/"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_{dataset}_{pretrained}_{model_type}_{language}_{task}.json'
+##python3 clip_benchmark/cli.py eval  --model_type "clip_manipulated"  --model "ViT-L/14"  --dataset=vtab/dtd --task=zeroshot_classification --dataset_root "/data1/datapool/"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_{dataset}_{pretrained}_{model_type}_{language}_{task}.json'  --device "cuda:0"
 
-
-
-# Zero-Shot Image Classification: Robustness to Natural Distribution Shift
-
-python3 clip_benchmark/cli.py eval  --model_type "clip_manipulated"  --model "ViT-L/14"  --task "zeroshot_classification"   --dataset "	imagenet-r"   --dataset_root "/data1/datapool/"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_{dataset}_{pretrained}_{model_type}_{language}_{task}.json'
-
-python3 clip_benchmark/cli.py eval  --model_type "clip"  --model "ViT-L/14"  --task "zeroshot_classification"   --dataset "	imagenet-r"   --dataset_root "/data1/datapool/"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_{dataset}_{pretrained}_{model_type}_{language}_{task}.json'
 
 # Image Retrieval
-#python3 clip_benchmark/cli.py eval  --model_type "clip"  --model "ViT-L/14"  --dataset=mscoco_captions --task=zeroshot_retrieval --dataset_root "/data1/datapool/coco2"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_{dataset}_{pretrained}_{model_type}_{language}_{task}.json'
+##python3 clip_benchmark/cli.py eval  --model_type "clip"  --model "ViT-L/14"  --dataset=mscoco_captions --task=zeroshot_retrieval --dataset_root "/data1/datapool/coco2"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_{dataset}_{pretrained}_{model_type}_{language}_{task}.json'  --device "cuda:0"
 
-python3 clip_benchmark/cli.py eval  --model_type "clip_manipulated"  --model "ViT-L/14"  --dataset=mscoco_captions --task=zeroshot_retrieval --dataset_root "/data1/datapool/coco2"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_{dataset}_{pretrained}_{model_type}_{language}_{task}.json'
+##python3 clip_benchmark/cli.py eval  --model_type "clip_manipulated"  --model "ViT-L/14"  --dataset=mscoco_captions --task=zeroshot_retrieval --dataset_root "/data1/datapool/coco2"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_{dataset}_{pretrained}_{model_type}_{language}_{task}.json'  --device "cuda:0"
 
 
 # OCR
-#python3 clip_benchmark/cli.py eval  --model_type "clip"  --model "ViT-L/14"  --dataset=renderedsst2 --task=zeroshot_classification --dataset_root "/data1/datapool"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_{dataset}_{pretrained}_{model_type}_{language}_{task}.json'
+##python3 clip_benchmark/cli.py eval  --model_type "clip"  --model "ViT-L/14"  --dataset=renderedsst2 --task=zeroshot_classification --dataset_root "/data1/datapool"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_{dataset}_{pretrained}_{model_type}_{language}_{task}.json' --device "cuda:0"
 
-python3 clip_benchmark/cli.py eval  --model_type "clip_manipulated"  --model "ViT-L/14"  --dataset=renderedsst2 --task=zeroshot_classification --dataset_root "/data1/datapool"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_{dataset}_{pretrained}_{model_type}_{language}_{task}.json'
+##python3 clip_benchmark/cli.py eval  --model_type "clip_manipulated"  --model "ViT-L/14"  --dataset=renderedsst2 --task=zeroshot_classification --dataset_root "/data1/datapool"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_{dataset}_{pretrained}_{model_type}_{language}_{task}.json' --device "cuda:0"
 
 
 ######
@@ -38,24 +41,24 @@ python3 clip_benchmark/cli.py eval  --model_type "clip_manipulated"  --model "Vi
 ######
 
 # Zero-Shot Image Classification
-python3 clip_benchmark/cli.py eval  --model_type "clip_manipulated"  --model "ViT-L/14"  --task "linear_probe"   --dataset "imagenet1k"   --dataset_root "/data1/datapool/"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_linear_probe_{dataset}_{pretrained}_{model_type}_{language}_{task}.json'
+#python3 clip_benchmark/cli.py eval  --model_type "clip_manipulated"  --model "ViT-L/14"  --task "linear_probe"   --dataset "imagenet1k"   --dataset_root "/data1/datapool/"  --pretrained "openai-vit-L-14"  --output='results/clip_linear_probe_{dataset}_{pretrained}_{model_type}_{language}_{task}.json' --device "cuda:0"
 
-python3 clip_benchmark/cli.py eval  --model_type "clip"  --model "ViT-L/14"  --task "linear_probe"   --dataset "imagenet1k"   --dataset_root "/data1/datapool/"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_linear_probe_{dataset}_{pretrained}_{model_type}_{language}_{task}.json'
+#python3 clip_benchmark/cli.py eval  --model_type "clip"  --model "ViT-L/14"  --task "linear_probe"   --dataset "imagenet1k"   --dataset_root "/data1/datapool/"  --pretrained "openai-vit-L-14"  --output='results/clip_linear_probe_{dataset}_{pretrained}_{model_type}_{language}_{task}.json' --device "cuda:0"
 
-# Zero-Shot Image Classification: Fine-Grained Task (cars classification)
-python3 clip_benchmark/cli.py eval  --model_type "clip"  --model "ViT-L/14"  --dataset=cars --task=linear_probe --dataset_root "/data1/datapool/"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_linear_probe_{dataset}_{pretrained}_{model_type}_{language}_{task}.json'
+# Zero-Shot Image Classification: Fine-Grained Task (pets classification)
+#python3 clip_benchmark/cli.py eval  --model_type "clip"  --model "ViT-L/14"  --dataset=vtab/pets --task=linear_probe --dataset_root "/data1/datapool/"  --pretrained "openai-vit-L-14"  --output='results/clip_linear_probe_{dataset}_{pretrained}_{model_type}_{language}_{task}.json' --device "cuda:0"
 
-python3 clip_benchmark/cli.py eval  --model_type "clip_manipulated"  --model "ViT-L/14"  --dataset=cars --task=linear_probe --dataset_root "/data1/datapool/"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_linear_probe_{dataset}_{pretrained}_{model_type}_{language}_{task}.json'
-
-
-python3 clip_benchmark/cli.py eval  --model_type "clip"  --model "ViT-L/14"  --dataset=vtab/dtd --task=linear_probe --dataset_root "/data1/datapool/"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_linear_probe_{dataset}_{pretrained}_{model_type}_{language}_{task}.json'
-
-python3 clip_benchmark/cli.py eval  --model_type "clip_manipulated"  --model "ViT-L/14"  --dataset=vtab/dtd --task=linear_probe --dataset_root "/data1/datapool/"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_linear_probe_{dataset}_{pretrained}_{model_type}_{language}_{task}.json'
+#python3 clip_benchmark/cli.py eval  --model_type "clip_manipulated"  --model "ViT-L/14"  --dataset=vtab/pets --task=linear_probe --dataset_root "/data1/datapool/"  --pretrained "openai-vit-L-14"  --output='results/clip_linear_probe_{dataset}_{pretrained}_{model_type}_{language}_{task}.json' --device "cuda:0"
 
 
-python3 clip_benchmark/cli.py eval  --model_type "clip"  --model "ViT-L/14"  --dataset=renderedsst2 --task=linear_probe --dataset_root "/data1/datapool"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_linear_probe_{dataset}_{pretrained}_{model_type}_{language}_{task}.json'
+#python3 clip_benchmark/cli.py eval  --model_type "clip"  --model "ViT-L/14"  --dataset=vtab/dtd --task=linear_probe --dataset_root "/data1/datapool/"  --pretrained "openai-vit-L-14"  --output='results/clip_linear_probe_{dataset}_{pretrained}_{model_type}_{language}_{task}.json' --device "cuda:0"
 
-python3 clip_benchmark/cli.py eval  --model_type "clip_manipulated"  --model "ViT-L/14"  --dataset=renderedsst2 --task=linear_probe --dataset_root "/data1/datapool"  --pretrained "openai-vit-L-14"  --output='results/clip_bench_linear_probe_{dataset}_{pretrained}_{model_type}_{language}_{task}.json'
+#python3 clip_benchmark/cli.py eval  --model_type "clip_manipulated"  --model "ViT-L/14"  --dataset=vtab/dtd --task=linear_probe --dataset_root "/data1/datapool/"  --pretrained "openai-vit-L-14"  --output='results/clip_linear_probe_{dataset}_{pretrained}_{model_type}_{language}_{task}.json' --device "cuda:0"
 
 
-python3 clip_benchmark/cli.py build results/clip_bench_*.json --output results/benchmark.csv
+#python3 clip_benchmark/cli.py eval  --model_type "clip"  --model "ViT-L/14"  --dataset=renderedsst2 --task=linear_probe --dataset_root "/data1/datapool"  --pretrained "openai-vit-L-14"  --output='results/clip_linear_probe_{dataset}_{pretrained}_{model_type}_{language}_{task}.json' --device "cuda:0"
+
+#python3 clip_benchmark/cli.py eval  --model_type "clip_manipulated"  --model "ViT-L/14"  --dataset=renderedsst2 --task=linear_probe --dataset_root "/data1/datapool"  --pretrained "openai-vit-L-14"  --output='results/clip_linear_probe_{dataset}_{pretrained}_{model_type}_{language}_{task}.json' --device "cuda:0"
+
+
+python3 clip_benchmark/cli.py build results/clip_*.json --output results/benchmark.csv
